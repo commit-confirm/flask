af2519e1a1f623dfe50eccfbffc3cb54d958633d
Lee <lee-2098@live.co.uk>
2019-01-13 12:42:34 +0000

    chapter 2 complete

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
-@app.route('/hello_world')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Lee'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]    
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
